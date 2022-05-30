98c8ba063e4d62e98ed272786c223269b7587649
RogerVazquezNieves <rogelio.vn@sjuanrio.tecnm.mx>
2022-05-30 18:06:49 -0500

    Create ejemplo_operadores.ino

---
+++ ejemplo_operadores/ejemplo_operadores.ino
@@ -0,0 +1,29 @@
+/*
+ * Estructura básica
+ * Por: cristina sanchez
+ * Fecha: 11 de abril
+ * 
+ * Esto es una muestra de la estructura básica de un programa
+ */
+
+// Bibliotecas
+
+// Constantes
+
+// Variables
+
+// Definición de objetos
+
+// Condiciones iniciales - Se ejecuta sólo una vez al energizar
+void setup() {// Inicio de void setup ()
+  // Aquí va tu código
+
+}// Fin de void setup
+
+// Cuerpo del programa - Se ejecuta constamente
+void loop() {// Inicio de void loop
+  // put your main code here, to run repeatedly:
+
+}// Fin de void loop
+
+// Funcioes del usuario
